de111433414c41d4065f1a0439fa72cc55f7b992
aonghusflynn <developer@aonghusflynn.com>
2013-02-21 20:53:21 +0000

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ireland::Application.config.secret_token = 'a70b56a43724b421077533d54b25e45d878f8d2d19e89420f959bf7fc9c1ba1c9364494b3c4b3a76fb378302f1ffadcc52de4532d1a21161d4e2db8f145944a7'
